8e178c802546492fa07e9564428f66537725b0d7
Carol <contatocarolinefeijo@gmail.com>
2022-11-10 10:54:54 -0300

    website nails

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import './App.css'
+
+export default function App() {
+  return <div className="App">
+  <h1>aqui é a APP</h1>
+  </div>
+}
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <div>
+      <h1>ola</h1>
+    </div>
+  )
+}
