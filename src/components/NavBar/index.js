2c92fcd39e896f6c0fc1f11bd2bc006ff01428a8
Carol <contatocarolinefeijo@gmail.com>
2022-11-18 11:22:33 -0300

    create button navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './styles.css'
 import { Link } from 'react-router-dom'
 
-import './styles.css'
 import logo from '../../assets/img/logo.jpg'
+import menu from '../../assets/icons/menu.png'
+
 import ButtonContact from '../ButtonContact'
 
 export const NavBar = () => {
+  const [isActiveButton, setIsActiveButton] = useState(true)
   return (
     <nav className="navbar">
+      <link rel="stylesheet" type="text/css" href="estilo.css" />
+
       <Link to="/">
         <img className="logo" src={logo} alt="Liziane Silva" />
       </Link>
@@ -33,6 +38,31 @@ export const NavBar = () => {
           <ButtonContact />
         </li>
       </ul>
+
+      <nav>
+        <ul className= {!isActiveButton ? "menuDisable":  "menu" }>
+          <li>
+            <button onClick={()=> {setIsActiveButton(isActiveButton ? false : true)}}>
+              <img src={menu} alt="logo" />
+            </button>
+
+            <ul>
+              <li>
+                <a href="./">HOME</a>
+              </li>
+              <li>
+                <a href="./services">SERVIÇOS</a>
+              </li>
+              <li>
+                <a href="#">QUEM SOU</a>
+              </li>
+              <li>
+                <a href="#">LOCALIZAÇÃO</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
     </nav>
   )
 }
